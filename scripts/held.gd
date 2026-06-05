9e874d0a56c363a09bb6620253d927f3737b963d
Tempusername-ops <omar.asm.2006@gmail.com>
2026-06-05 20:33:57 +0300

    Revert "Merge branch 'main' into branch"
    This reverts commit 60326bf5babff569cada883296f47854e9312e6d, reversing
    changes made to e45e022064aefb133f6e428c7f0ffa0e7474ea83.

--- scripts/held.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-func _ready() -> void:
-	Global.change_held_count.connect(on_held_change)
-	text = "Held: 0/%d" % Global.maxInventory
-	
-func on_held_change(held: int, maxInv: int):
-	text = "Held: %d/%d" % [held, maxInv]
